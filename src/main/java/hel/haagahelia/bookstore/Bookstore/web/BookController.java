c382869ea7ab9ab52e7b52b471fe4a1e3ac35b00
BenjaminDecaillet <decaillet.benjamin@gmail.com>
2018-09-07 15:22:34 +0300

    Bookstore add and delete

--- src/main/java/hel/haagahelia/bookstore/Bookstore/web/BookController.java
+++ src/main/java/hel/haagahelia/bookstore/Bookstore/web/BookController.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +31,23 @@ public class BookController {
 	public String booklist(Model model) {
 		model.addAttribute("books", (List<Book>) bookrepository.findAll());
 	return "booklist";
+	}
+	
+	@RequestMapping(value = "/addbook")
+	public String addbook(Model model) {
+		model.addAttribute("book", new Book());
+	return "addbook";
+	}
+	
+	@RequestMapping(value = "/saveBook", method = RequestMethod.POST)
+	public String savebook(Book book) {
+		bookrepository.save(book);
+	return "redirect:booklist";
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteStudent(@PathVariable("id") Long bookId, Model model) {
+		bookrepository.deleteById(bookId);
+	return "redirect:../booklist";
 	}
 }
