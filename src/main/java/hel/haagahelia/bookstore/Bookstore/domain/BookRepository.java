ff3bb418a63343a103d022aa02d6bf4d362ccd56
BenjaminDecaillet <decaillet.benjamin@gmail.com>
2018-10-05 16:16:33 +0300

    Restfull methods GET POST DELETE created and functional

--- src/main/java/hel/haagahelia/bookstore/Bookstore/domain/BookRepository.java
+++ src/main/java/hel/haagahelia/bookstore/Bookstore/domain/BookRepository.java
@@ -3,18 +3,18 @@ package hel.haagahelia.bookstore.Bookstore.domain;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-
+@RepositoryRestResource
 public interface BookRepository extends CrudRepository<Book, Long> {
+	
+	List<Book> findByAuthor(@Param("author") String author);
+	
+	List<Book> findByTitle(@Param("title") String title);
 
-	//Book findById(long id);
+	List<Book> findByPrice(@Param("price") double price);
 	
-	List<Book> findByAuthor(String author);
-	
-	List<Book> findByTitle(String title);
-
-	List<Book> findByPrice(double price);
-	
-	List<Book> findByYear(int year);
+	List<Book> findByYear(@Param("year") int year);
 
 }
--- src/main/java/hel/haagahelia/bookstore/Bookstore/web/BookController.java
+++ src/main/java/hel/haagahelia/bookstore/Bookstore/web/BookController.java
@@ -1,88 +1,109 @@
 package hel.haagahelia.bookstore.Bookstore.web;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import hel.haagahelia.bookstore.Bookstore.domain.Book;
 import hel.haagahelia.bookstore.Bookstore.domain.BookRepository;
 import hel.haagahelia.bookstore.Bookstore.domain.CategoryRepository;
 
 
-@Controller
+@RestController
 public class BookController {
-	
+
 	static String createISBN(){
 		int x = (int)(Math.random()*((100000000-1000000)+1))+1000000;
 		int y = (int)(Math.random()*((100-10)+1))+10;
 		return x + "-"+y;
 	}
-	
+
 	@Autowired 
 	private BookRepository bookrepository;
 	@Autowired
 	private CategoryRepository categoryrepository;
-	
+
+
 	@RequestMapping(value={"/index"})
 	public String messages(Model model) {
 		return "redirect:index";
 	}
-	
+
 	@RequestMapping(value = "/booklist")
 	public String booklist(Model model) {
 		model.addAttribute("books", (List<Book>) bookrepository.findAll());
-	return "booklist";
+		return "booklist";
 	}
-	
+
 	@RequestMapping(value = "/addbook")
 	public String addbook(Model model) {
 		model.addAttribute("book", new Book());
 		model.addAttribute("categories", categoryrepository.findAll());
-	return "addbook";
+		return "addbook";
 	}
-	
+
 	@RequestMapping(value = "/editbook/{id}", method = RequestMethod.GET)
 	public String editbook(@PathVariable("id") Long bookId, Model model) {
 		model.addAttribute("book", bookrepository.findById(bookId));
 		model.addAttribute("categories", categoryrepository.findAll());
-	return "editbook";
+		return "editbook";
 	}
-	
+
 	@RequestMapping(value = "/saveBook", method = RequestMethod.POST)
 	public String savebook(Book book) {
 		if(book.getIsbn() == null)
 			book.setIsbn(createISBN());
 		bookrepository.save(book);
-	return "redirect:booklist";
+		return "redirect:booklist";
 	}
-	
+
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteStudent(@PathVariable("id") Long bookId, Model model) {
 		bookrepository.deleteById(bookId);
-	return "redirect:../booklist";
+		return "redirect:../booklist";
 	}
-	
+
 	/*
 	 * REST METHODS
 	 */
-	
+
 	@RequestMapping(value = "/books", method = RequestMethod.GET)
 	public @ResponseBody List<Book> bookList() {
-	return (List<Book>) bookrepository.findAll();
+		return (List<Book>) bookrepository.findAll();
 	}
-	
+
 	@RequestMapping(value = "/book/{id}", method = RequestMethod.GET)
 	public @ResponseBody Book findBook(@PathVariable("id") Long bookId) {
-	return bookrepository.findById(bookId).get();
+		return bookrepository.findById(bookId).get();
+	}
+	
+	@RequestMapping(value = "/book/{id}", method = RequestMethod.DELETE)
+	public String deletebook(@PathVariable("id") Long bookId) {
+		bookrepository.deleteById(bookId);
+		return "book with id "+ bookId + " deleted!";
+	}
+	
+	@RequestMapping(value = "/book", method = RequestMethod.POST)
+	public ResponseEntity<Object> createBook(@RequestBody Book book) {
+		Book savedBook = bookrepository.save(book);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(savedBook.getId()).toUri();
+
+		return ResponseEntity.created(location).build();
 	}
 }
